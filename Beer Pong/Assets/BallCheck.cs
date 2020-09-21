4d1bc8e6ee6e552ae5ca532613479c30fde2d642
juarezgabriel <juarezg@usc.edu>
2020-09-21 10:13:08 -0700

    added project

---
+++ Beer Pong/Assets/BallCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCheck : MonoBehaviour
+{
+    public GameObject thisCup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ball")
+        {
+            Debug.Log("in!");
+            thisCup.SetActive(false);
+        }
+    }
+
+}
